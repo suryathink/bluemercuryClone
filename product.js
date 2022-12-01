44d9ded4ee80e8cc686dfb976d07172336ab6230
suryathink <suryaprakashsingh110@gmail.com>
2022-12-01 15:43:15 +0530

    fetching-userName-at-ProductPage

--- product.js
+++ product.js
@@ -1,10 +1,12 @@
 var cartArr= JSON.parse(localStorage.getItem("cart")) || [];
 document.getElementById("cart-count").textContent = cartArr.length;
-  // let x=localStorage.getItem("user")
-  // document.getElementById("showEmailUser").textContent = x;
-// // if(document.getElementById("cart-count")){
-// //   document.getElementById("cart-count").textContent = cartArr.length;
-// // }
+let x=localStorage.getItem("user");
+
+if (x == null){
+  document.getElementById("showEmailUser").textContent = "SIGN IN/UP";
+} else {
+  document.getElementById("showEmailUser").textContent = x;
+}
 var mensData = [
   {
     image_url:
